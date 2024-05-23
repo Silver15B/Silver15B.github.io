0a7dcb8e82619270b6a54332d0939101737664bf
Silver15B <baylyt106@gmail.com>
2024-05-24 09:55:52 +1000

    Removed default react app stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SyntheticEvent, useState } from 'react';
 
 function App() {
+  const [textarea, setTextarea] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setTextarea(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={textarea} onChange={handleChange}></input>
     </div>
   );
 }
